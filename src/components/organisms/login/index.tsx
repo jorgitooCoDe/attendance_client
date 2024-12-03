bb1a3ab961d6a69489029f689935b04b2cddbd4e
DevJorgito <jorgito.soh@gmail.com>
2024-12-02 19:26:43 -0500

    remove sign up side

--- src/components/organisms/login/index.tsx
+++ src/components/organisms/login/index.tsx
@@ -1,17 +1,13 @@
 import React from 'react';
 import LoginForm from '../../molecules/loginForm';
-import WelcomeMessage from '../../molecules/welcomeMessage';
 
 const Login: React.FC = () => {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen">
       <h2 className="mb-8 text-2xl font-bold font-main_sans text-mto_gray">Attendance</h2>
-      <div className="flex w-3/5 h-auto rounded-lg shadow-custom-red">
-        <div className="flex-1 flex items-center justify-center p-8 bg-white">
+      <div className="flex w-full max-w-lg h-auto rounded-lg shadow-custom-red">
+        <div className="flex items-center justify-center w-full p-8 bg-white">
           <LoginForm />
-        </div>
-        <div className="flex-1 p-8 bg-mto_gray">
-          <WelcomeMessage />
         </div>
       </div>
     </div>
