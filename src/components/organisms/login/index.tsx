9af021dffa9a9386c57299407945834d09454940
DevJorgito <jorgito.soh@gmail.com>
2024-12-07 18:50:07 -0500

    add changes in login

--- src/components/organisms/login/index.tsx
+++ src/components/organisms/login/index.tsx
@@ -4,7 +4,7 @@ import LoginForm from '../../molecules/loginForm';
 const Login: React.FC = () => {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen">
-      <h2 className="mb-8 text-2xl font-bold font-main_sans text-mto_gray">Attendance</h2>
+      <h2 className="mb-8 text-2xl font-bold font-main_sans text-mto_gray select-none">Attendance</h2>
       <div className="flex w-full max-w-lg h-auto rounded-lg shadow-custom-red">
         <div className="flex items-center justify-center w-full p-8 bg-white">
           <LoginForm />
