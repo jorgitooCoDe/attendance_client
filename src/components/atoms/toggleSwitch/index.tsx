c9b7b5c2cf926ae238c7a29818cccd0e127e4349
DevJorgito <jorgito.soh@gmail.com>
2024-12-08 18:37:08 -0500

    add style for toggle switch

--- src/components/atoms/toggleSwitch/index.tsx
+++ src/components/atoms/toggleSwitch/index.tsx
@@ -9,13 +9,15 @@ const ToggleSwitch: React.FC<ToggleSwitchProps> = ({ isChecked, onToggle }) => {
   return (
     <button
       onClick={onToggle}
-      className={`w-12 h-6 flex items-center bg-gray-300 rounded-full p-1 transition-colors duration-300 focus:outline-none ${isChecked ? 'bg-green-500' : 'bg-gray-300'
-        }`}
+      className={`w-20 h-8 flex items-center bg-gray-300 rounded-full p-1 transition-colors duration-300 focus:outline-none ${isChecked ? 'bg-mto_red_light' : 'bg-gray-300'
+        } relative`}
     >
+      <span className={`text-sm absolute left-2 ${isChecked ? 'text-white' : 'text-gray-500'}`}>Sí</span>
       <div
-        className={`bg-white w-4 h-4 rounded-full shadow-md transform duration-300 ${isChecked ? 'translate-x-6' : 'translate-x-0'
+        className={`bg-white w-6 h-6 rounded-full shadow-md transform duration-300 ${isChecked ? 'translate-x-12' : 'translate-x-0'
           }`}
       ></div>
+      <span className={`text-sm absolute right-2 ${isChecked ? 'text-white' : 'text-gray-500'}`}>No</span>
     </button>
   );
 };
