4fca11b2d3ff1652159f672c202954a64d34700f
DevJorgito <jorgito.soh@gmail.com>
2024-12-08 16:50:18 -0500

    add tertiary variant in button prop

--- src/components/atoms/button/index.tsx
+++ src/components/atoms/button/index.tsx
@@ -3,19 +3,35 @@ import React from 'react';
 type ButtonProps = {
   text: string;
   onClick: () => void;
-  variant?: 'primary' | 'secondary';
+  variant?: 'primary' | 'secondary' | 'tertiary';
+  className?: string;
 };
 
-const Button: React.FC<ButtonProps> = ({ text, onClick, variant = 'primary' }) => {
-  const baseClasses = "py-2 mt-4 rounded transition-colors hover:bg-mto_red rounded-2xl text-white";
-  const primaryClasses = "w-full bg-mto_gray text-center mx-auto";
-  const secondaryClasses = "w-2/3 border bg-mto_red hover:bg-mto_gray";
+const Button: React.FC<ButtonProps> = ({ text, onClick, variant = 'primary', className = '' }) => {
+  const baseClasses = "py-2 mt-4 rounded transition-colors focus:outline-none";
 
-  const classes = `${baseClasses} ${variant === 'primary' ? primaryClasses : secondaryClasses}`;
+  const primaryClasses = "w-full bg-mto_gray text-center mx-auto hover:bg-mto_dark_gray text-white";
+  const secondaryClasses = "w-2/3 border bg-mto_red hover:bg-mto_dark_red text-white";
+  const tertiaryClasses = "bg-transparent text-mto_gray underline hover:text-red-500";
 
-  if (variant !== 'primary' && variant !== 'secondary') {
-    console.error(`Invalid variant prop: ${variant}. Expected 'primary' or 'secondary'.`);
+  let variantClasses = '';
+
+  switch (variant) {
+    case 'primary':
+      variantClasses = primaryClasses;
+      break;
+    case 'secondary':
+      variantClasses = secondaryClasses;
+      break;
+    case 'tertiary':
+      variantClasses = tertiaryClasses;
+      break;
+    default:
+      console.error(`Invalid variant prop: ${variant}. Expected 'primary', 'secondary', or 'tertiary'.`);
+      variantClasses = primaryClasses;
   }
+
+  const classes = `${baseClasses} ${variantClasses} ${className}`;
 
   return (
     <button onClick={onClick} className={classes}>
